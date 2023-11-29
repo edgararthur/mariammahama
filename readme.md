61cd60ca228070c71a590c2d365e77802651c18a
edgararthur <gilfoyle.arthur@protonmail.com>
2023-11-29 18:01:32 +0000

    initial commit

---
+++ readme.md
@@ -0,0 +1,22 @@
+Create a website using HTML, JavaScript, and CSS. It is optional to include XML in your coding.
+The website must contain 3 pages; Homepage, App page, and Contact Us page. A page should have
+a menu that allows access to the other two pages.
+
+Instructions
+Homepage:
+1. The homepage should contain information about your apps. The information should be
+displayed using video, audio, text, and images.
+2. Apart from the menu on the homepage, there should be a text link that sends you to the app
+page.
+3. Relate your web apps to two core values of HCC.
+App page:
+1. Using JavaScript, create two web apps.
+2. The App page should contain a picture and text representing the apps.
+3. JavaScript codes should not be written in the app.html page but linked to it. Example,
+Webapp.js is linked to App.html.
+Contact Us page:
+1. This page should contain a form for sending a message to the developer. The form should
+contain; User’s Name, User’s Email, Message box and Send/Submit button.
+2. The page should contain contact info of the website owner; Email and Telephone number.
+
+Finally, submit the mini project in a zipped/compressed# mariammahama
---
+++ webapp.js
@@ -0,0 +1,36 @@
+const closeBtn = document.getElementById('close');
+
+document.getElementById('app1').innerHTML = `
+    <div class="card-content">
+        <p>Foster a sense of community by creating a platform where individuals with shared convictions can connect, share ideas, and collaborate..</p>
+        <img src="./a86a258a-059e-49da-8ea9-6f46a9302ab2.webp" alt="">
+        <a href="#" id="playVideo">
+            <span><i class="fa-solid fa-circle-play"></i></span>Play Video 
+        </a>
+        <video id="myVideo" controls>
+            <source src="WELCOME TO A WORLD OF OPPORTUNITIES.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+        <span class="close"><i class="fa-solid fa-close"></i></span>
+    </div>
+`;
+
+
+document.getElementById('app2').innerHTML = `
+    <div class="card-content">
+        <p>Empower individuals or groups with strong convictions to raise funds for projects aligned with their values.</p>
+        <!-- Add more content as needed -->
+    </div>
+`;
+
+document.getElementById('playVideo').addEventListener('click', function() {
+    var video = document.getElementById('myVideo');
+    video.style.display = 'block'; // Show the video
+    video.play();
+}, false);
+
+closeBtn.addEventListener('click', () => {
+    var video = document.getElementById('myVideo');
+    video.style.display = 'none'; // Hide the video
+    video.pause();
+})
