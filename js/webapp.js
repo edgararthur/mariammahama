c790b2aa3b36a57050deaf230889f7f1b87492b6
edgararthur <gilfoyle.arthur@protonmail.com>
2023-12-01 17:20:36 +0000

    upadted

--- js/webapp.js
+++ js/webapp.js
@@ -3,7 +3,7 @@ document.getElementById("app1").innerHTML = `
     <div class="card-content">
         <p>Foster a sense of community by creating a platform where individuals with shared convictions can connect, share ideas, and collaborate..</p>
         <img src="./a86a258a-059e-49da-8ea9-6f46a9302ab2.webp" alt="">
-        <a href="#" id="playVideo">
+        <a href="#" class="playVideo">
             <span><i class="fa-solid fa-circle-play"></i></span>Play Video 
         </a>
         <video id="myVideo" controls>
@@ -28,7 +28,7 @@ document.getElementById("app2").innerHTML = `
     </div>
 `;
 
-document.querySelector("#playVideo").addEventListener(
+document.querySelector(".playVideo").addEventListener(
 	"click",
 	function () {
 		var video = document.getElementById("myVideo");
